4e5edb3474a345a7df6c33315e9b3d9169346d73
chen-rongliang <chen-rongliang@qq.com>
2022-09-20 17:37:02 +0800

    Update blog.zxx.user.js

--- blog.zxx.user.js
+++ blog.zxx.user.js
@@ -4,27 +4,19 @@
 // @version      1.0.0
 // @description  干掉广告
 // @author       MO
-// @create          2018-08-14
+// @create       2018-08-14
 // @match        *://www.zhangxinxu.com/wordpress*
-// @match        *://www.zhangxinxu.com/study*
 // @run-at       document-end
-// @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
 ;(function() {
     'use strict';
-    var el = {
-        root: $("#page")
-    }
-    if(!el.root.length) el.root = $("body")
-    el.root.find("#topAdOut").remove()
-    el.root.find('.topa').remove()
-    el.root.append('<style>.da_title,#respond,.da_col2,#respond ~ .navigation,.commentlist,.post,.navigation_top,.navigation_bot,#content .pagetitle{margin:auto}.navigation_bot{margin-top:36px}#ad,.bota{display:none!important;}@media(min-width:1600px){.narrowcolumn, .widecolumn{padding-right:15px}}</style>')
-    el.root.find("#daYidengFloat").remove()
-    el.root.find(".ins_txt").remove()
-    el.root.find("p.link + div.link").remove()
-    el.root.find(".da_article").remove()
-    el.root.find(".da_title").remove()
-    el.root.find(".da_col2").remove()
+    var lists = [
+        '.sign-intro + *',
+        '.adsbygoogle',
+        '.wp_share_box'
+    ];
+
+    document.body.insertAdjacentHTML('beforeEnd', '<style>'+ lists.join(',') +'{position:absolute!important;top:-1000px!important;left:-1000px!important}</style>');
 })()
